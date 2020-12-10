5578026ae527daa779f004b1de5713f30b4d6592
Tony-Hsuan <a5827251@gmail.com>
2020-12-10 22:34:04 +0800

    Update 2020-12-10T14:33:52.115Z

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+// console.log('Hello!');
+// $(document).ready(() => {
+//   console.log('HesSchool Hello!');
+// });
+// $(document).ready(function () {
+//   $('.sidebar-content li').click(function (e) { 
+//     e.preventDefault();
+//       $(this).toggleClass('active');
+//       $(this).siblings().removeClass('active')
+//   });
+// });
+"use strict";
+//# sourceMappingURL=all.js.map
